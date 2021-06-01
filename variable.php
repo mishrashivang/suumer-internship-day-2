7755e5fff6511e83251edb8ffaa7a06c6615740b
mishrashivang <85113719+mishrashivang@users.noreply.github.com>
2021-06-01 15:42:41 +0530

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php
+$a = 2;
+switch($a){
+    case 1 :
+        echo "one";
+        break;
+    case 2 :
+        echo "two";
+        break;
+    case 3 :
+        echo "three";
+        break;
+        
+    default :
+    echo "please select 1-3";
+}
+?>
